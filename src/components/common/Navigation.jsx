abcc82ff6842315c00efca1c8c2b247a979dac35
Mohamed Ali <mohamedalix546@gmail.com>
2026-02-09 03:44:50 +0200

    Branding: Finalize duck logo and fix navigation syntax

--- src/components/common/Navigation.jsx
+++ src/components/common/Navigation.jsx
@@ -127,6 +127,7 @@ export default function Navigation() {
                   );
                 })}
               </div>
+            </div>
           </motion.div>
         </div>
       </motion.nav>
